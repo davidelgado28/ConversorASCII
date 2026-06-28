d82383ba31ad97fffb5a3932bde1a53fbea4c3f4
David Carlos <davidcarlosdelgado28@gmail.com>
2026-06-28 13:40:45 -0300

    Create conversor.cpp

---
+++ conversor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    cout << (char)n << "\n";
+    
+    return 0;
+}
