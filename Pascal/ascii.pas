a2c34625b6d49d627f53eb6f6a6c02ab55bf600c
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-31 22:03:57 -0300

    Create ascii.pas

---
+++ Pascal/ascii.pas
@@ -0,0 +1,7 @@
+$numero = [int] (Read-Host "Digite um numero (0-127)")
+
+if (numero−lt0−ornumero -lt 0 -ornumero−lt0−ornumero -gt 127) {
+    Write-Host "Valor invalido! Use 0 a 127."
+} else {
+    Write-Host "ASCII: ([char]([char]([char]numero)"
+} 
